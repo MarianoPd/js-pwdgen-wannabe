446a1d98ec3bc45f10b438a8540313ff4d087204
MarianoPd <marianopidone.com>
2021-10-20 15:37:02 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const firstname = prompt("Qual è il tuo nome ?");
+const surname = prompt("E il tuo cognome ?");
+const favouriteColor = prompt("Il tuo colore preferito ?");
+
+const superRandomNumber = 21;
+
+console.log(firstname);
+console.log(surname);
+console.log(favouriteColor);
+
+let secretPassword = firstname + surname + favouriteColor + superRandomNumber;
+
+let passwordLeight = secretPassword.length;
+
+let outputS =
+`
+    <span>Your new password is:</span><br>
+    <h1>${secretPassword}</h1>
+    <span> Please use it carefully </span>  
+
+`
+document.getElementById('output').innerHTML = outputS;
